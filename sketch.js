dc4aef84b070858260c2144b56b58da13ac74922
AakarshN <77533089+AakarshN@users.noreply.github.com>
2021-05-17 15:46:57 +0530

    updated

--- sketch.js
+++ sketch.js
@@ -55,11 +55,10 @@ function draw() {
   bob3.display();
   bob4.display();
   bob5.display();
-  
-  keypressed();
+ 
  
 }
-function keypressed(){
+function keyPressed(){
 	if(keyCode===UP_ARROW){
 		Matter.Body.applyForce(bob1.body, bob1.body.position,{x:-50,y:-45});
 	}
